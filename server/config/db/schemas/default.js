ebb38c27ec1432c44b9ce5e39a5fa77468a4e5da
Mark Lilien <mark@marklilien.com>
2025-12-27 10:27:47 -0800

    test: adding test db configuration

--- server/config/db/schemas/default.js
+++ server/config/db/schemas/default.js
@@ -19,44 +19,6 @@ module.exports = {
             "unique": false
         }
     },
-    "users": {
-        "id": {
-            "type": "UUID",
-            "allowNull": false,
-            "primaryKey": true,
-            "unique": true
-        },
-        "name": {
-            "type": "VARCHAR(255)",
-            "allowNull": false,
-            "primaryKey": false,
-            "unique": false
-        },
-        "email": {
-            "type": "VARCHAR(255)",
-            "allowNull": false,
-            "primaryKey": false,
-            "unique": true
-        },
-        "api_token": {
-            "type": "VARCHAR(255)",
-            "allowNull": false,
-            "primaryKey": false,
-            "unique": true
-        },
-        "created_at": {
-            "type": "DATETIME",
-            "allowNull": false,
-            "primaryKey": false,
-            "unique": false
-        },
-        "updated_at": {
-            "type": "DATETIME",
-            "allowNull": false,
-            "primaryKey": false,
-            "unique": false
-        }
-    },
     "mats": {
         "id": {
             "type": "UUID",
@@ -98,6 +60,44 @@ module.exports = {
         "creator_id": {
             "type": "UUID",
             "allowNull": true,
+            "primaryKey": false,
+            "unique": false
+        }
+    },
+    "users": {
+        "id": {
+            "type": "UUID",
+            "allowNull": false,
+            "primaryKey": true,
+            "unique": true
+        },
+        "name": {
+            "type": "VARCHAR(255)",
+            "allowNull": false,
+            "primaryKey": false,
+            "unique": false
+        },
+        "email": {
+            "type": "VARCHAR(255)",
+            "allowNull": false,
+            "primaryKey": false,
+            "unique": true
+        },
+        "api_token": {
+            "type": "VARCHAR(255)",
+            "allowNull": false,
+            "primaryKey": false,
+            "unique": true
+        },
+        "created_at": {
+            "type": "DATETIME",
+            "allowNull": false,
+            "primaryKey": false,
+            "unique": false
+        },
+        "updated_at": {
+            "type": "DATETIME",
+            "allowNull": false,
             "primaryKey": false,
             "unique": false
         }
