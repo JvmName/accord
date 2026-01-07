396baad1e0916ad17d98887426f4f8c44b7abc7d
Parth Padgaonkar <parth@jvmname.dev>
2026-01-06 17:14:35 -0800

    bump version

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -120,8 +120,8 @@ android {
         applicationId = "com.rdojo.kombat"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
     }
     signingConfigs {
         create("release") {
