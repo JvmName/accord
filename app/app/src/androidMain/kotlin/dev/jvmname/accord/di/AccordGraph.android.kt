274e6082f1f816a94692697aca7a904133258b5d
Parth Padgaonkar <parth@jvmname.dev>
2026-04-10 13:53:49 -0700

    buildconfig + better base URL mgmt

--- app/app/src/androidMain/kotlin/dev/jvmname/accord/di/AccordGraph.android.kt
+++ app/app/src/androidMain/kotlin/dev/jvmname/accord/di/AccordGraph.android.kt
@@ -1,5 +1,6 @@
 package dev.jvmname.accord.di
 
+import accord.app.BuildConfig
 import dev.jvmname.accord.network.BaseUrl
 
-actual fun platformBaseUrl(): BaseUrl = BaseUrl("https://rdk.api.jvmname.dev")
+actual fun platformBaseUrl(): BaseUrl = BaseUrl(BuildConfig.BASE_URL)
