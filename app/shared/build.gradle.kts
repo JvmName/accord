69b0bc48da2fd246a5d9a04e275792c0d530036d
Parth Padgaonkar <parth@jvmname.dev>
2026-04-15 10:02:51 -0700

    shared: simplify android block for KMP library

--- app/shared/build.gradle.kts
+++ app/shared/build.gradle.kts
@@ -142,42 +142,7 @@ kotlin {
 android {
     namespace = "dev.jvmname.accord"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
-
-    defaultConfig {
-        applicationId = "com.rdojo.kombat"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 6
-        versionName = "0.0.6"
-    }
-    signingConfigs {
-        create("release") {
-            storeFile = file(System.getenv("SIGNING_STORE_FILE") ?: "AccordProdKey.jks")
-            storePassword = System.getenv("SIGNING_STORE_PASSWORD")
-            keyAlias = System.getenv("SIGNING_KEY_ALIAS")
-            keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
-        }
-    }
-    buildTypes {
-        getByName("debug") {
-            isMinifyEnabled = false
-            isShrinkResources = false
-
-        }
-        release {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            signingConfig = signingConfigs.getByName("release")
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
-    }
+    minSdk = libs.versions.android.minSdk.get().toInt()
 }
 
 
