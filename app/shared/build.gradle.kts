50f99287075a75155cfe2964b538a9a146ac458c
Parth Padgaonkar <parth@jvmname.dev>
2026-04-15 10:02:32 -0700

    shared: swap androidApplication → androidLibrary plugin

--- app/shared/build.gradle.kts
+++ app/shared/build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.composeHotReload)
