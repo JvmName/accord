7067c49bccb2172a8cc1e7160db49d095da16c90
Parth Padgaonkar <parth@jvmname.dev>
2026-04-20 18:27:49 -0700

    keep screen on

--- app/shared/src/commonMain/kotlin/dev/jvmname/accord/ui/session/judging/JudgeSessionContent.kt
+++ app/shared/src/commonMain/kotlin/dev/jvmname/accord/ui/session/judging/JudgeSessionContent.kt
@@ -41,6 +41,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.keepScreenOn
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -88,7 +89,7 @@ fun JudgeSessionContent(state: JudgeSessionState, modifier: Modifier) {
     }
     AudioPlayback(state.matchState.audio)
 
-    Box(modifier = modifier.fillMaxSize()) {
+    Box(modifier = modifier.keepScreenOn().fillMaxSize()) {
         StandardScaffold(
             modifier = Modifier
                 .fillMaxSize()
