61b7695548847d64fa47b1bc0372e8f5c5507bcc
Parth Padgaonkar <parth@jvmname.dev>
2026-04-21 09:09:42 -0700

    bump to 1.0.0-rc02, fix build issue on android

--- app/androidApp/build.gradle.kts
+++ app/androidApp/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.rdojo.kombat"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 100
-        versionName = "1.0.0-rc01"
+        versionCode = 101
+        versionName = "1.0.0-rc02"
     }
 
     signingConfigs {
