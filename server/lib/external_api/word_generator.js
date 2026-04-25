96a322c4a9af37cfdb0c6ba6155bf8cc12f5172a
Parth Padgaonkar <parth@jvmname.dev>
2026-04-24 23:10:44 -0500

    remove external API dependency

--- server/lib/external_api/word_generator.js
+++ server/lib/external_api/word_generator.js
@@ -1,21 +1,862 @@
-const { HttpClient } = require('../http/client');
-
-
-class WordGenerator extends HttpClient {
-
-    async getWords(count) {
-        const language      = 'en';
-        const type          = 'lowercase';
-        const words         = count;
-        const category      = 'animals';
-        const requestParams = { language, type, words, category};
-        const result        = await this.get('/api', { requestParams });
-        return result.data.map(word => word.word.replace(/\s+/, '_'));
+const WORDS = [
+    {
+        "word": "lion",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "tiger",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "elephant",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "giraffe",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "zebra",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "monkey",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "panda",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "koala",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "kangaroo",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "dolphin",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "bear",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "wolf",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "fox",
+        "length": 3,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "leopard",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "cheetah",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "rhinoceros",
+        "length": 10,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "hippopotamus",
+        "length": 12,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "deer",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "camel",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "goat",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "sheep",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "cow",
+        "length": 3,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "buffalo",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "horse",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "donkey",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "pig",
+        "length": 3,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "dog",
+        "length": 3,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "cat",
+        "length": 3,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "rabbit",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "mouse",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "rat",
+        "length": 3,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "squirrel",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "bat",
+        "length": 3,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "otter",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "beaver",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "raccoon",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "skunk",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "moose",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "antelope",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "reindeer",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "chimpanzee",
+        "length": 10,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "gorilla",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "orangutan",
+        "length": 9,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "whale",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "shark",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "seal",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "walrus",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "penguin",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "flamingo",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "ostrich",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "eagle",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "hawk",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "falcon",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "owl",
+        "length": 3,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "sparrow",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "parrot",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "peacock",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "swan",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "crow",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "pigeon",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "turkey",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "chicken",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "duck",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "goose",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "lizard",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "crocodile",
+        "length": 9,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "alligator",
+        "length": 9,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "snake",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "frog",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "toad",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "turtle",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "tortoise",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "crab",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "lobster",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "octopus",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "jellyfish",
+        "length": 9,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "starfish",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "seahorse",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "bee",
+        "length": 3,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "ant",
+        "length": 3,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "butterfly",
+        "length": 9,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "moth",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "spider",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "scorpion",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "mosquito",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "fly",
+        "length": 3,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "dragonfly",
+        "length": 9,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "ladybug",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "snail",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "worm",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "manatee",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "narwhal",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "blue whale",
+        "length": 10,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "humpback whale",
+        "length": 14,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "orca",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "beluga",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "stingray",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "manta ray",
+        "length": 9,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "eel",
+        "length": 3,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "moray eel",
+        "length": 9,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "clownfish",
+        "length": 9,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "tuna",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "salmon",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "trout",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "carp",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "catfish",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "bass",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "pike",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "anchovy",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "sardine",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "guppy",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "goldfish",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "betta",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "angelfish",
+        "length": 9,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "lionfish",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "goby",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "lamprey",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "crayfish",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "plankton",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "krill",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "sea cucumber",
+        "length": 12,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "sea urchin",
+        "length": 10,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "barnacle",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "coral",
+        "length": 5,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "sea sponge",
+        "length": 10,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "nudibranch",
+        "length": 10,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "newt",
+        "length": 4,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "axolotl",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "mudskipper",
+        "length": 10,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "flatfish",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "halibut",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "mackerel",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "marlin",
+        "length": 6,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "barracuda",
+        "length": 9,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "grouper",
+        "length": 7,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "flounder",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "sea turtle",
+        "length": 10,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "blue tang",
+        "length": 9,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "zebra fish",
+        "length": 10,
+        "category": "animals",
+        "language": "en"
+    },
+    {
+        "word": "sailfish",
+        "length": 8,
+        "category": "animals",
+        "language": "en"
     }
+];
 
 
-    get urlRoot() {
-        return 'https://random-words-api.kushcreates.com';
+class WordGenerator {
+
+    getWords(count) {
+        const picked = [];
+        const used = new Set();
+
+        while (picked.length < count && picked.length < WORDS.length) {
+            const idx = Math.floor(Math.random() * WORDS.length);
+            if (!used.has(idx)) {
+                used.add(idx);
+                picked.push(WORDS[idx].word.replace(/\s+/, '_'));
+            }
+        }
+
+        return picked;
     }
 }
 
